6548fa8f323884045255be5cfd816ad31ba6db31
Murillo de Morais <murillo.morais@vianuvem.com.br>
2019-06-18 11:41:26 -0300

    feat: initial commit

---
+++ src/components/PageTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PageTitle = styled.h1`
+  color: red;
+`;
+
+export default PageTitle;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import PageTitle from '../components/PageTitle';
+
+const Home = () => {
+  return (
+    <PageTitle>Teste</PageTitle>
+  )
+}
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default Home;
